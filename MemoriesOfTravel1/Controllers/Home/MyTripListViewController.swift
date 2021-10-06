4f09ba19546ed31bdd0a900121c27de7e1d8f9cd
LeeHa-Yeon <gkdus1613@naver.com>
2021-10-07 01:34:28 +0900

    Initial Commit

---
+++ MemoriesOfTravel1/Controllers/Home/MyTripListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  MemoriesOfTravel1
+//
+//  Created by 이하연 on 2021/10/07.
+//
+
+import UIKit
+
+class MyTripListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
